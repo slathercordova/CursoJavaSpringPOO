518299c1656340a9ee80db06cb809ffa3243a261
Slather <slathercordova@gmail.com>
2026-02-06 22:03:10 -0500

    06/02/2026

---
+++ CursoJavaSpringPOO/src/Main.java
@@ -0,0 +1,8 @@
+import pooTeoria.Usuario;
+
+public class Main {
+    static void main(String[] args) {
+        Usuario usuario1 = new Usuario();
+        System.out.println(usuario1);
+    }
+}
---
+++ CursoJavaSpringPOO/src/pooTeoria/Usuario.java
@@ -0,0 +1,9 @@
+package pooTeoria;
+
+public class Usuario {
+    String username;
+    String email;
+    int edad;
+    String sexo;
+    String pais;
+}
