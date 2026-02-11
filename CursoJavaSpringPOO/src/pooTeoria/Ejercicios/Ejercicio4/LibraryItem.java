b165eb654c60599fb034ba72dd0293705225e4b2
SlatherEss <slathercordova@gmail.com>
2026-02-11 16:15:04 -0500

    11/02/2026

--- CursoJavaSpringPOO/src/pooTeoria/Ejercicios/Ejercicio4/LibraryItem.java
+++ CursoJavaSpringPOO/src/pooTeoria/Ejercicios/Ejercicio4/LibraryItem.java
@@ -1,5 +1,39 @@
 package pooTeoria.Ejercicios.Ejercicio4;
 
 public class LibraryItem {
+    private String title;
+    private int itemID;
+    private boolean isLoaned;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(int itemID) {
+        this.itemID = itemID;
+    }
+
+    public boolean getIsLoaned() {
+        return isLoaned;
+    }
+
+    public void setLoaned(boolean loaned) {
+        isLoaned = loaned;
+    }
+
+    public String showDetails() {
+        return "LibraryItem{" +
+                "title='" + title + '\'' +
+                ", itemID=" + itemID +
+                ", isLoaned=" + isLoaned +
+                '}';
+    }
 }
